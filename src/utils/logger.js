0db3339a21343b77cb00a2fd5f4070ec0671267d
ZiadGamalDev <zyadgamal450@gmail.com>
2025-03-11 22:32:25 +0200

    refactor: update logger configuration to use console transport and set log level to info

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,12 +6,16 @@ dotenv.config();
 const silent = process.env.APP_DEBUG?.toLowerCase().trim() === "false";
 
 const logger = createLogger({
+  level: "info",
   silent,
   format: format.combine(
     format.timestamp(),
     format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
   ),
-  transports: [new transports.File({ filename: "storage/logs/app.log" })],
+  transports: [
+    // new transports.File({ filename: "storage/logs/app.log" }),
+    new transports.Console(),
+  ],
 });
 
 export default logger;
