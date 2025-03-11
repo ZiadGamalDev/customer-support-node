32899cee0c2f259a121de65cee4d15190f6111d2
ZiadGamalDev <zyadgamal450@gmail.com>
2025-03-11 23:03:43 +0200

    fix: update profile routes to include email verification for show and update actions

--- src/modules/profile/profile.routes.js
+++ src/modules/profile/profile.routes.js
@@ -4,10 +4,11 @@ import ProfileValidation from './profile.validation.js';
 import validate from '../../middleware/validate.js';
 import authenticate from '../../middleware/authenticate.js';
 import upload from '../../middleware/upload.js';
+import verify from '../../middleware/verify.js';
 
 const profileRoutes = Router();
 
-profileRoutes.get('/', authenticate('user'), ProfileController.show);
-profileRoutes.put('/', authenticate('user'), validate(ProfileValidation.updateProfile), upload.single('image'), ProfileController.update);
+profileRoutes.get('/', authenticate('user'), verify('email'), ProfileController.show);
+profileRoutes.put('/', authenticate('user'), verify('email'), validate(ProfileValidation.updateProfile), upload.single('image'), ProfileController.update);
 
 export default profileRoutes;
