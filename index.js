5e7bcb4b83e8755bc1d07c0c48a008d8e81c1221
ZiadGamalDev <zyadgamal450@gmail.com>
2025-03-11 18:45:42 +0200

    update init

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import app from './src/app.js';
 import connectDB from './src/database/connection.js';
-import { Server } from 'socket.io';
-import resourceSockets from './src/resource/sockets.js';
 
 dotenv.config();
 
@@ -11,8 +9,4 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
-let server = app.listen(PORT, () => console.log(`Server running at ${BASE_URL}`));
-
-const io = new Server(server, { cors: '*' });
-
-// io.on('connection', (socket) => resourceSockets(socket, io));
+app.listen(PORT, () => console.log(`Server running at ${BASE_URL}`));
--- src/resource/listeners.js
+++
@@ -1,7 +0,0 @@
-import eventEmitter from "../utils/event.emitter.js";
-import logger from "../utils/logger.js";
-// import sendMessage from "../modules/chat/chat.event.js";
-
-// eventEmitter.on("messageSent", sendMessage);
-
-logger.info("Event listeners are registered");
